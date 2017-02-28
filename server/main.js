1a53e0f1f98b1a2c10183d0be11ea4f6b210a939
Vachirakorn Rungsikawanich <vachirakorn@hotmail.com>
2017-03-01 02:20:20 +0700

    no message

--- server/main.js
+++ server/main.js
@@ -5,29 +5,22 @@ const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+
 const IS_DEV = process.env.NODE_ENV !== 'production';
 if(IS_DEV) {
   // setup dev here
 }
 
-
-
 // setup express
 const app = express();
 const connection = require('./helpers/connection');
-app.use(require('./routes'));
 
+app.use(require('./routes'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: process.env.SESSION_SECRET,
-  store: new MongoStore({
-    url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
-    autoReconnect: true
-  })
-}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
