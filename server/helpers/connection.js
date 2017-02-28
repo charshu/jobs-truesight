47a4297fdbad22595cff258aef35c78518fe80f8
Vachirakorn Rungsikawanich <vachirakorn@hotmail.com>
2017-02-28 22:00:40 +0700

    no message

--- server/helpers/connection.js
+++ server/helpers/connection.js
@@ -1,6 +1,22 @@
 // TODO: connection to mongodb
 const mongoose = require('mongoose');
 
+const MONGODB_URI= 'mongodb://localhost:27017/test';
+const MONGOLAB_URI= 'mongodb://localhost:27017/test';
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGODB_URI || MONGOLAB_URI);
+mongoose
+  .connection
+  .on('error', () => {
+    console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+    process.exit();
+  });
+
+
 module.exports = function connect() {
   // connect to mongoDB
   const MONGO_URI = process.env.MONGOURI;
